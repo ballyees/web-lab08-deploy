4c07ddd40afe9a54f2b5b30582f3c18d0898c9d2
ballyees <kittinan_prompao@elearning.cmu.ac.th>
2020-03-20 22:34:36 +0700

    Updates

---
+++ precache-manifest.b85a34e1a8ebc3efc94e9e768a6d0b59.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e0690d477cbfbab76c49441c485ef8",
+    "url": "/web-lab08-deploy/index.html"
+  },
+  {
+    "revision": "2bf67d18863dcd0a7439",
+    "url": "/web-lab08-deploy/static/css/2.62ebf70e.chunk.css"
+  },
+  {
+    "revision": "fb2171634c66a79ce0ee",
+    "url": "/web-lab08-deploy/static/css/main.0b974771.chunk.css"
+  },
+  {
+    "revision": "2bf67d18863dcd0a7439",
+    "url": "/web-lab08-deploy/static/js/2.1f251358.chunk.js"
+  },
+  {
+    "revision": "9cc59aa88bbb965336535eb6b360773d",
+    "url": "/web-lab08-deploy/static/js/2.1f251358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb2171634c66a79ce0ee",
+    "url": "/web-lab08-deploy/static/js/main.f80dcae4.chunk.js"
+  },
+  {
+    "revision": "8a1409e0d47e99e54c6b",
+    "url": "/web-lab08-deploy/static/js/runtime-main.cb30d6fe.js"
+  },
+  {
+    "revision": "cb83a55ca9b912aa11d18153b5d58758",
+    "url": "/web-lab08-deploy/static/media/dev.cb83a55c.jfif"
+  }
+]);
--- src/component/month.js
+++
@@ -1,4 +0,0 @@
-var m = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-export default function month_map(month) {
-    return m[month]
-}
